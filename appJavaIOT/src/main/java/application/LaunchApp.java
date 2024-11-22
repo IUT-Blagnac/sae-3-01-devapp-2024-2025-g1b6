c2b2f76a5a1c4ab2f53258117d8bc0666511ee25
Yassir Boulouiha <133369221+yssrbnl@users.noreply.github.com>
2024-11-22 09:30:27 +0100

    Mise à jour LaunchApp + petite modif FXML

--- appJavaIOT/src/main/java/application/LaunchApp.java
+++ appJavaIOT/src/main/java/application/LaunchApp.java
@@ -10,9 +10,8 @@ import java.io.IOException;
 public class LaunchApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LaunchApp.class.getResource("view/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(LaunchApp.class.getResource("view/RoomsView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
