f30ef35a497a99664e948a7dda5f28c22cb44361
Alex LOVIN <144910136+imalexlov1@users.noreply.github.com>
2024-12-18 17:58:13 +0100

    Modif avec tri

--- public_html/SP_RECHERCHE_AVANCEE.sql
+++ public_html/SP_RECHERCHE_AVANCEE.sql
@@ -1,23 +1,24 @@
 DELIMITER $$
-
-CREATE PROCEDURE SP_RECHERCHE_PRODUITS(
-    IN mot_cle VARCHAR(255),
-    IN categorie INT,
-    IN marque VARCHAR(255),
-    IN prix_min DECIMAL(10, 2),
-    IN prix_max DECIMAL(10, 2),
-    IN en_stock BOOLEAN,
-    IN limit_count INT,
-    IN offset_count INT
+CREATE DEFINER=`R2024MYSAE3012`@`%` PROCEDURE `SP_RECHERCHE_AVANCEE`(
+    IN `mot_cle` VARCHAR(255),
+    IN `categorie` INT,
+    IN `marque` VARCHAR(255),
+    IN `prix_min` DECIMAL(10,2),
+    IN `prix_max` DECIMAL(10,2),
+    IN `en_stock` BOOLEAN,
+    IN `limit_count` INT,
+    IN `offset_count` INT,
+    IN `tri` VARCHAR(50)
 )
 BEGIN
-    SELECT DISTINCT p.IDPROD, 
-                    p.NOMPROD, 
-                    p.DESCPROD, 
-                    p.COULEUR,
-                    p.PRIXHT, 
-                    p.QTESTOCK, 
-                    m.NOMMARQUE
+    SELECT DISTINCT 
+        p.IDPROD, 
+        p.NOMPROD, 
+        p.DESCPROD, 
+        p.COULEUR,
+        p.PRIXHT, 
+        p.QTESTOCK, 
+        m.NOMMARQUE
     FROM PRODUIT p
     LEFT JOIN MARQUE m ON p.IDMARQUE = m.IDMARQUE
     LEFT JOIN APPARTENIRCATEG ap ON ap.IDPROD = p.IDPROD
@@ -33,7 +34,15 @@ BEGIN
         AND (p.PRIXHT >= IFNULL(prix_min, p.PRIXHT) OR prix_min IS NULL)
         AND (p.PRIXHT <= IFNULL(prix_max, p.PRIXHT) OR prix_max IS NULL)
         AND (p.QTESTOCK > 0 OR en_stock IS NULL)
+    ORDER BY 
+        CASE 
+            WHEN tri = 'nom_asc' THEN p.NOMPROD
+            WHEN tri = 'prix_asc' THEN p.PRIXHT
+        END ASC,
+        CASE 
+            WHEN tri = 'nom_desc' THEN p.NOMPROD
+            WHEN tri = 'prix_desc' THEN p.PRIXHT
+        END DESC
     LIMIT limit_count OFFSET offset_count;
 END$$
-
 DELIMITER ;
